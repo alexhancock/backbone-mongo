67ced513b17b6668210c1ea2518e8f5324642e94
Alex Hancock <alex@alexhancock.com>
2013-07-27 11:49:18 -0700

    [queries/modifiers] Adding support for a good number of the query and update functions

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-
 var collection = new Backbone.MongoCollection();
 
-collection.insert({ name: 'Alex', score: 10 });
-collection.insert({ name: 'Claire', score: 20 });
+// collection.insert([{ name: 'Alex', score: 10 }, { name: 'Claire', score: 20 }]);
+collection.insert({ name: 'Ben', score: 3 });
+collection.insert([{ name: 'Claire', score: 3 }, {name:'Al', score: 20}]);
 
-// collection.sync('UPDATE', collection);
-debugger;
-collection.remove([{ name: 'Alex' }, { name: 'Claire' }]);
-debugger;
+collection.store.find({ score: { $gt: 50 }}).observe({
+  added: function(doc){
+    console.log(doc);
+  }
+});
+
+setTimeout(function(){
+  var ben = collection.findOne({ name: 'Ben' });
+  ben.set('score', 100);
+}, 4000);
+
+/* 
+ 
+collection.comparator = function(doc){
+  return doc.get('score');
+};
+
+// collection.add({ name: 'Ben', score: 30 }, { at: 1 });
+var all = collection.find({ score: { '$gt' : 1 }});
+
+
+*/
