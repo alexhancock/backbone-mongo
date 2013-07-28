07b988ecede2abddaf1d7b9cdcd44e41835d303e
Alex Hancock <alex@alexhancock.com>
2013-07-27 17:41:04 -0700

    [updates] Dynamic updates

--- app.js
+++ app.js
@@ -2,27 +2,11 @@ var collection = new Backbone.MongoCollection();
 
 // collection.insert([{ name: 'Alex', score: 10 }, { name: 'Claire', score: 20 }]);
 collection.insert({ name: 'Ben', score: 3 });
-collection.insert([{ name: 'Claire', score: 3 }, {name:'Al', score: 20}]);
+collection.insert([{ name: 'Claire', score: 5 }, {name:'Alex', score: 20}]);
 
-collection.store.find({ score: { $gt: 50 }}).observe({
-  added: function(doc){
-    console.log(doc);
-  }
-});
+collection.update({ name: 'Ben' }, { $inc: { score: 10 }, $set: { name: 'George' } });
 
 setTimeout(function(){
-  var ben = collection.findOne({ name: 'Ben' });
-  ben.set('score', 100);
+  var george = collection.findOne({ name: 'George' });
+  george.set('score', 100);
 }, 4000);
-
-/* 
- 
-collection.comparator = function(doc){
-  return doc.get('score');
-};
-
-// collection.add({ name: 'Ben', score: 30 }, { at: 1 });
-var all = collection.find({ score: { '$gt' : 1 }});
-
-
-*/
