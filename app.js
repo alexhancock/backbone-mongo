614a6c01caacff31dd0ad8fe8e0a0065fd95af47
Alex Hancock <alex@alexhancock.com>
2013-07-28 11:30:39 -0700

    [objects] Store smaller objects in the actual LocalCollection

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var collection = new Backbone.MongoCollection();
 
 // collection.insert([{ name: 'Alex', score: 10 }, { name: 'Claire', score: 20 }]);
 collection.insert({ name: 'Ben', score: 3 });
-collection.insert([{ name: 'Claire', score: 5 }, {name:'Alex', score: 20}]);
+collection.insert({ name: 'Alex', score: 4 });
+collection.insert([{ name: 'Claire', score: 5 }, { name:'Alex', score: 20 }]);
 
-collection.update({ name: 'Ben' }, { $inc: { score: 10 }, $set: { name: 'George' } });
+collection.update({ name: 'Alex' }, { $inc: { score: 10 }, $set: { name: 'George' } }, { multi: true });
 
 setTimeout(function(){
   var george = collection.findOne({ name: 'George' });
